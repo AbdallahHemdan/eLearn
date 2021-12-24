34d673b8cfcecf6eebec1501a38b0f5bec9511e7
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2021-12-24 17:31:48 +0200

    feat(Fogot): :sparkles: add fogot page wrapper

---
+++ e-learn/src/views/forgot.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container auth">
+    <div class="auth__wrapper">
+      <form-header
+        href="/login"
+        main-title="Forgot your password ?"
+        second-title="back to login page"
+      />
+
+      <or-divider />
+
+      <forgot-form />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Forgot',
+  components: {
+    ForgotForm: () => import('@/components/auth/ForgotForm/forgot-form.vue'),
+    OrDivider: () => import('@/components/common/OrDivider/OrDivider.vue'),
+    FormHeader: () => import('@/components/auth/FormHeader/form-header.vue'),
+  },
+};
+</script>
