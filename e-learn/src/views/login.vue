c51240fd9f2a78fee49423535eb9173e8d7ca35c
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2021-12-24 17:33:00 +0200

    refactor(Login): :recycle: refactor login page

--- e-learn/src/views/login.vue
+++ e-learn/src/views/login.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="container c-auth-wrapper">
-    <div class="login-form">
+  <div class="container auth">
+    <div class="auth__wrapper">
       <form-header
         href="/signup"
         main-title="Sign in to your account"
@@ -15,31 +15,12 @@
 </template>
 
 <script>
-import OrDivider from '../components/common/OrDivider/OrDivider.vue';
-import FormHeader from '../components/auth/FormHeader/form-header.vue';
-import LoginForm from '../components/auth/LoginForm/login-form.vue';
-
 export default {
   name: 'Login',
-  components: { FormHeader, OrDivider, LoginForm },
+  components: {
+    LoginForm: () => import('@/components/auth/LoginForm/login-form.vue'),
+    OrDivider: () => import('@/components/common/OrDivider/OrDivider.vue'),
+    FormHeader: () => import('@/components/auth/FormHeader/form-header.vue'),
+  },
 };
 </script>
-
-<style lang="scss" scoped>
-.login-form {
-  min-height: 576px;
-  max-width: 28rem;
-  width: 100%;
-}
-
-.c-auth-wrapper {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-height: 100%;
-  padding-top: 3rem;
-  padding-bottom: 3rem;
-  padding-left: 1rem;
-  padding-right: 1rem;
-}
-</style>
