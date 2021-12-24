9365d44fb12e2b3608967846c512e911a4c8aae1
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2021-12-24 16:39:44 +0200

    feat(navbar): :sparkles: add profile dropdown component

---
+++ e-learn/src/components/common/ProfileDropdown/ProfileDropdown.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="dropdown profile-options">
+    <a
+      class="dropdown-toggle"
+      type="button"
+      id="dropdownMenuButton"
+      data-toggle="dropdown"
+      aria-haspopup="true"
+      aria-expanded="false"
+    >
+      <img :src="userInfo.image" class="profile-options__img" alt="profile" />
+    </a>
+
+    <div
+      class="dropdown-menu dropdown-menu-end right-dropdown"
+      aria-labelledby="dropdownMenuButton"
+    >
+      <a class="dropdown-item" :href="`/profile/${userInfo.id}`">
+        <img src="@/assets/svgs/avatar.svg" alt="avatar image" class="profile-options__icon" />
+        Profile
+      </a>
+      <a class="dropdown-item" href="/settings">
+        <img src="@/assets/svgs/settings.svg" alt="settings icon" class="profile-options__icon" />
+        Settings
+      </a>
+
+      <div class="logout">
+        <a class="dropdown-item" href="/" @click="logout">
+          <img src="@/assets/svgs/logout.svg" alt="logout icon" class="profile-options__icon" />
+          Log Out
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getUserInfo } from '@/utilities/user';
+
+export default {
+  name: 'ProfileDropdown',
+  data: function () {
+    return {
+      userInfo: '',
+    };
+  },
+  methods: {
+    logout: function () {},
+    setUserInfo: function () {
+      this.userInfo = getUserInfo();
+    },
+  },
+  mounted() {
+    this.setUserInfo();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.right-dropdown {
+  position: absolute;
+  right: 0px;
+}
+
+@import '@/components/common/navbar/navbar.scss';
+</style>
