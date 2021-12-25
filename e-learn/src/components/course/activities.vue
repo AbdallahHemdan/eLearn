9c9fb266c11c5cda726ade82f7c018aac4f46b64
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2021-12-25 02:25:38 +0200

    feat: :sparkles: add placeholder for qa and activities pages

---
+++ e-learn/src/components/course/activities.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">Activities Component</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ e-learn/src/components/course/qa.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">QA Component</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
