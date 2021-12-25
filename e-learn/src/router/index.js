e800647b884c3dd30398cb1acd97e617074d4fb9
EmanOthman21 <eothman21@gmail.com>
2021-12-25 21:56:53 +0200

    feat(Manage Users): :sparkles: add scrolling and enhance UI

--- e-learn/src/router/index.js
+++ e-learn/src/router/index.js
@@ -8,7 +8,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import(/* webpackChunkName: "home" */ '@/views/home.vue'),
+    component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
   },
   {
     path: '/login',
--- e-learn/src/views/ManageUsers.vue
+++ e-learn/src/views/ManageUsers.vue
@@ -1,26 +1,29 @@
 <template>
   <div class="manage-container">
-    <table class="table">
+    <table class="table table-borderless table-fixed">
       <thead>
         <tr>
-          <th scope="col">Users</th>
+          <th scope="col" class="table__title">Users</th>
         </tr>
       </thead>
       <thead>
-        <tr>
+        <tr class="table__header">
           <th scope="col">Username</th>
           <th scope="col">Role</th>
           <th scope="col">Action</th>
         </tr>
       </thead>
-      <tbody>
-        <tr v-for="(user, index) in users" :key="index" class="table__row">
-          <td>{{ user.username }}</td>
+      <tbody class="table__body">
+        <tr v-for="(user, index) in users" :key="index" class="table__row user">
+          <td>
+            <img src="@/assets/svgs/avatar.svg" alt="" class="user__img" />
+            {{ user.username }}
+          </td>
           <td>{{ user.type }}</td>
           <td>
             <button
               type="submit"
-              class="btn btn-block upgrade-btn"
+              class="btn btn-block user__upgrade-btn"
               @click.prevent="upgrade(user.username)"
               v-if="user.type == 'learner'"
             >
@@ -67,6 +70,46 @@ export default {
               username: "Adel",
               type: "instructor",
             },
+            {
+              username: "Hemdan",
+              type: "learner",
+            },
+            {
+              username: "Adel",
+              type: "instructor",
+            },
+            {
+              username: "Hemdan",
+              type: "learner",
+            },
+            {
+              username: "Adel",
+              type: "instructor",
+            },
+            {
+              username: "Hemdan",
+              type: "learner",
+            },
+            {
+              username: "Adel",
+              type: "instructor",
+            },
+            {
+              username: "Hemdan",
+              type: "learner",
+            },
+            {
+              username: "Adel",
+              type: "instructor",
+            },
+            {
+              username: "Hemdan",
+              type: "learner",
+            },
+            {
+              username: "Adel",
+              type: "instructor",
+            },
           ];
         });
     },
@@ -92,17 +135,55 @@ export default {
 
 <style lang="scss" scoped>
 .manage-container {
-  margin: 100px;
+  margin: 80px auto;
+  border: solid 3px $white-smoke;
+
+  // To add scroll bar to the table
+  table tbody {
+    display: block;
+    max-height: 400px;
+    overflow-y: scroll;
+  }
+  table thead,
+  table tbody tr {
+    display: table;
+    width: 100%;
+    table-layout: fixed;
+  }
 
   .table {
-    &__row {
-      .upgrade-btn {
-        padding: 10px;
-        background-color: $main-color;
-        color: $white;
+    &__title {
+      padding: 18px 15px;
+      font-weight: 600;
+      font-size: 20px;
+    }
 
-        &:hover {
-          background-color: $sub-color;
+    &__header {
+      background-color: $white-smoke;
+      th {
+        padding: 15px;
+        font-weight: 600;
+      }
+    }
+
+    &__row {
+      border-bottom: 2px solid $white-smoke;
+
+      .user {
+        &__img {
+          width: 30px;
+          height: 30px;
+          margin: 0 5px;
+        }
+
+        &__upgrade-btn {
+          padding: 5px 10px;
+          background-color: $main-color;
+          color: $white;
+
+          &:hover {
+            background-color: $sub-color;
+          }
         }
       }
     }
