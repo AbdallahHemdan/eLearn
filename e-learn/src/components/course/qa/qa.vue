062258de319536efeb15a554758b1be19ba4e67b
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2021-12-30 17:13:59 +0200

    feat(qa): :sparkles: add the form of new post

--- e-learn/src/components/course/qa/qa.vue
+++ e-learn/src/components/course/qa/qa.vue
@@ -1,8 +1,29 @@
 <template>
   <div class="container">
     <div class="new-question">
-      <img src="@/assets/svgs/avatar.svg" alt="user image" class="user__image" />
-      <button class="post" @click="openModal">What is on your mind?</button>
+      <div class="question">
+        <img src="@/assets/svgs/avatar.svg" alt="user image" class="user__image" />
+
+        <div class="question__content">
+          <textarea
+            name="question"
+            class="form-control"
+            id="question"
+            placeholder="What is on your mind?"
+            v-model="question"
+          />
+          <div class="actions">
+            <button
+              type="button"
+              class="btn btn-outline-secondary post__btn"
+              :disabled="!question"
+              @click.prevent="post"
+            >
+              Post
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -10,6 +31,11 @@
 <script>
 export default {
   name: 'QA',
+  data() {
+    return {
+      question: '',
+    };
+  },
   methods: {
     openModal() {
       console.log('Open Modal');
@@ -24,7 +50,7 @@ export default {
   border-radius: 10px;
   border: 1px solid rgba(108, 122, 137, 0.25);
   display: flex;
-  align-items: center;
+  flex-direction: column;
 }
 
 .user__image {
@@ -41,5 +67,32 @@ export default {
   background-color: #f0f2f547;
   border-radius: 16px;
   color: gray;
+
+  &__btn {
+    color: $main-color;
+    border-color: $main-color;
+    width: -webkit-fill-available;
+
+    &:hover {
+      background-color: $sub-color;
+      color: $white;
+    }
+  }
+}
+
+.question {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.question__content {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
+.actions {
+  margin-top: 8px;
 }
 </style>
