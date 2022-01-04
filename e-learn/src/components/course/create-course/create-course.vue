4872e6fd8694f03e69e3803ff437417832df7e25
EmanOthman21 <eothman21@gmail.com>
2022-01-04 13:45:21 +0200

    feat: :sparkles: update courses list without refreshing

--- e-learn/src/components/course/create-course/create-course.vue
+++ e-learn/src/components/course/create-course/create-course.vue
@@ -132,7 +132,7 @@ export default {
           }
         )
         .then((response) => {
-          console.log(response);
+          this.$emit("createCourse", response.data);
           this.clearData();
           document.getElementById("close").click();
         })
